ded678184e9b56a684d13903c2ffc5e93c993597
abdulhafizxalimjonov <abdulhafizxalimjonov@gmail.com>
2025-07-18 12:16:46 +0500

    Initial commit

---
+++ src/main/kotlin/com/abdullkhafiz/productdisplaydemo/ProductDisplayDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.abdullkhafiz.productdisplaydemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductDisplayDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductDisplayDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/abdullkhafiz/productdisplaydemo/ProductDisplayDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.abdullkhafiz.productdisplaydemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductDisplayDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
